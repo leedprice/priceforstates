7838ccbfcb90d8dd093e625e7e3c7b27e0d510fb
deependerch <deepender07@gmail.com>
2016-12-16 14:16:27 +0530

    state funtionality Commit

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+  function print_country(country_id) {
+      
+      $("#country-icon").replaceWith("<i id=\"country-icon-dynamic\" class=\"fa fa-refresh fa-pulse\"></i>");
+      $.getJSON('https://leedonline-api.usgbc.org/v1/Common/getCountriesAndStates.json', function (data) {
+          var option_str = document.getElementById(country_id);
+          option_str.length = 0;
+          option_str.options[0] = new Option('Select Country', '');
+          option_str.selectedIndex = 0;
+          var countries = data.countries;
+          var printCountries = Object.keys(countries);
+          printCountries.forEach(function (key) {
+              option_str.options[option_str.length] = new Option(countries[key], key);
+          });
+          $("#country-icon-dynamic").replaceWith("<i id=\"country-icon\" class=\"fa fa-globe\"></i>");
+      });
+  }
+
+  function print_state(state_id, country_index) {
+      
+      $("#country-icon").replaceWith("<i id=\"country-icon-dynamic\" class=\"fa fa-refresh fa-pulse\"></i>");
+      $.getJSON('https://leedonline-api.usgbc.org/v1/Common/getCountriesAndStates.json', function (data) {
+          var option_str = document.getElementById(state_id);
+          option_str.length = 0;
+          option_str.options[0] = new Option('Select State', '');
+          option_str.selectedIndex = 0;
+          if (data.divisions[country_index] != undefined) {
+              $("#state-option").removeClass("hidden").addClass("visible");
+              $('#state').prop('required', true);
+              var states = data.divisions[country_index];
+              var printStates = Object.keys(states);
+              printStates.forEach(function (key) {
+                  option_str.options[option_str.length] = new Option(states[key], key);
+              });
+          }
+          else {
+              $("#state-option").removeClass("visible").addClass("hidden");
+              $("#state").removeAttr("required");
+          }
+          $("#country-icon-dynamic").replaceWith("<i id=\"country-icon\" class=\"fa fa-globe\"></i>");
+      });
+  }
+  $(document).ready(function () {
+      $("#leedprice").submit(function () {
+          $("#submit-calculate").replaceWith("<button id=\"submit-dynamic\" class=\"btn btn-danger\"><i class=\"fa fa-spinner fa-pulse\"></i> Calculating</button>");
+          var country = $("#country").val();
+          var state = $("#state").val();
+          var ratingSystem = $("#ratingSystem").val();
+          var type = $('#type').val();
+          var givenArea = $('#givenArea').val();
+          $.getJSON('https://leedonline-api.usgbc.org/v1/Common/getPriceRelatedInfo.json?countryOrCurrency=' + country, function (infodata) {
+              var currency = infodata.data.currency;
+              $.getJSON('https://leedonline-api.usgbc.org/v1/LEEDPricing/getVersionPricesInfo.json?versionOrCurrency=' + currency + '&ratingSystem=' + ratingSystem + '&area=' + givenArea + '&calculate=' + type, function (pricedata) {
+                  // console.log(pricedata.payableInfo.member.taxes.all.taxes);
+                  if (pricedata.payableInfo != undefined) {
+                      createTotalPriceTable(infodata, pricedata);
+                      createTaxTable(infodata, pricedata);
+                      createGrandTotal(infodata, pricedata);
+                      $("#modalpopup").modal({ backdrop: "static"});
+                  }
+                  else {
+                      $("#modalerror").modal({ backdrop: "static"});
+                  }
+                  $("#submit-dynamic").replaceWith("<button id=\"submit-calculate\" class=\"btn btn-primary\" type=\"submit\" name=\"submit\"><i class=\"fa fa-calculator\"></i> Calculate</button>");
+              });
+          });
+      });
+      $(".modal-close").click(function () {
+          $('#tablebody').empty();
+          $("#leedprice").trigger("reset");
+      });
+  });
+
+  function createTotalPriceTable(infodata, pricedata) {
+      var member = pricedata.payableInfo.member;
+      var nonMember = pricedata.payableInfo.nonMember;
+      var trow = document.createElement("tr");
+      var tdrow = document.createElement("th");
+      var textnode = document.createTextNode("Total");
+      tdrow.appendChild(textnode);
+      trow.appendChild(tdrow);
+      tdrow = document.createElement("td");
+      textnode = document.createTextNode(member.total);
+      tdrow.appendChild(textnode);
+      trow.appendChild(tdrow);
+      tdrow = document.createElement("td");
+      textnode = document.createTextNode(nonMember.total);
+      tdrow.appendChild(textnode);
+      trow.appendChild(tdrow);
+      document.getElementById("tablebody").appendChild(trow);
+  }
+
+  function createTaxTable(infodata, pricedata) {
+      var taxCodes = infodata.data.taxCodes;
+      var member = pricedata.payableInfo.member;
+      var nonMember = pricedata.payableInfo.nonMember;
+      var memTaxObjSize = Object.keys(member.taxes.all.taxes).length;
+      if (memTaxObjSize > 0 && memTaxObjSize != "undefined") {
+          var taxrow = document.createElement("tr");
+          var tdrow = document.createElement("th");
+          var textnode = document.createTextNode("Taxes");
+          tdrow.appendChild(textnode);
+          taxrow.appendChild(tdrow);
+          document.getElementById("tablebody").appendChild(taxrow);
+          var taxes = Object.keys(member.taxes.all.taxes);
+          taxes.forEach(function (key) {
+              var taxrow = document.createElement("tr");
+              var tdrow = document.createElement("td");
+              var textnode = document.createTextNode(taxCodes[key]);
+              tdrow.appendChild(textnode);
+              taxrow.appendChild(tdrow);
+              tdrow = document.createElement("td");
+              textnode = document.createTextNode(member.taxes.all.taxes[key]);
+              tdrow.appendChild(textnode);
+              taxrow.appendChild(tdrow);
+              tdrow = document.createElement("td");
+              textnode = document.createTextNode(nonMember.taxes.all.taxes[key]);
+              tdrow.appendChild(textnode);
+              taxrow.appendChild(tdrow);
+              document.getElementById("tablebody").appendChild(taxrow);
+          });
+      }
+  }
+
+  function createGrandTotal(infodata, pricedata) {
+      var member = pricedata.payableInfo.member;
+      var nonMember = pricedata.payableInfo.nonMember;
+      var grandTotalrow = document.createElement("tr");
+      var tdrow = document.createElement("th");
+      var textnode = document.createTextNode("Grand Total");
+      tdrow.appendChild(textnode);
+      grandTotalrow.appendChild(tdrow);
+      tdrow = document.createElement("th");
+      textnode = document.createTextNode(member.taxes.all.grandTotal);
+      tdrow.appendChild(textnode);
+      grandTotalrow.appendChild(tdrow);
+      tdrow = document.createElement("th");
+      textnode = document.createTextNode(nonMember.taxes.all.grandTotal);
+      tdrow.appendChild(textnode);
+      grandTotalrow.appendChild(tdrow);
+      document.getElementById("tablebody").appendChild(grandTotalrow);
+  }
